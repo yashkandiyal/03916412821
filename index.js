5b366e7f8f4c4540835a4198c9c26c5426aab8b3
Yash kandiyal <yashkandiyal22@gmail.com>
2024-07-11 20:07:28 +0530

    Added comments for better readability.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import axios from 'axios';
+import express from "express";
+import axios from "axios";
 const app = express();
 const port = 9876;
 
@@ -7,33 +7,35 @@ const windowSize = 10;
 let numberWindow = [];
 
 const thirdPartyAPIs = {
-  p: 'http://20.244.56.144/test/primes',  
-  f: 'http://20.244.56.144/test/fibo',    
-  e: 'http://20.244.56.144/test/even',    
-  r: 'http://20.244.56.144/test/rand',    
+  p: "http://20.244.56.144/test/primes",
+  f: "http://20.244.56.144/test/fibo",
+  e: "http://20.244.56.144/test/even",
+  r: "http://20.244.56.144/test/rand",
 };
 
 const fetchNumber = async (type) => {
   try {
-      const response = await axios.get(thirdPartyAPIs[type], { timeout: 500 }, {
+    const response = await axios.get(
+      thirdPartyAPIs[type],
+      { timeout: 500 },
+      {
         headers: {
-              'Content-Type': 'application/json',
-              'Accept': 'application/json'
-          },
-          
-    });
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
     if (response.data && response.data.number) {
       return response.data.number;
     }
   } catch (error) {
-      console.error(error);
-      
+    console.error(error);
   }
   return null;
 };
-
+//checking if the number is unique
 const isUnique = (number) => !numberWindow.includes(number);
-
+//updating the number window
 const updateWindow = (newNumber) => {
   if (numberWindow.length >= windowSize) {
     numberWindow.shift();
@@ -45,22 +47,22 @@ const calculateAverage = () => {
   const sum = numberWindow.reduce((acc, num) => acc + num, 0);
   return numberWindow.length ? sum / numberWindow.length : 0;
 };
-
-app.get('/numbers/:type', async (req, res) => {
-    const type = req.params.type;
-  const validTypes = ['p', 'f', 'e', 'r'];
+//get request
+app.get("/numbers/:type", async (req, res) => {
+  const type = req.params.type;
+  const validTypes = ["p", "f", "e", "r"];
 
   if (!validTypes.includes(type)) {
-    return res.status(400).send({ error: 'Invalid number type' });
+    return res.status(400).send({ error: "Invalid number type" });
   }
-
+  //initializing the number window
   const prevWindowState = [...numberWindow];
-
+  //fetching the number
   const newNumber = await fetchNumber(type);
   if (newNumber !== null && isUnique(newNumber)) {
     updateWindow(newNumber);
   }
-
+  //sending the response
   const avg = calculateAverage();
   const response = {
     windowPrevState: prevWindowState,
@@ -71,7 +73,7 @@ app.get('/numbers/:type', async (req, res) => {
 
   res.send(response);
 });
-
+//starting the server at the given port:9876
 app.listen(port, () => {
   console.log(`port running at http://localhost:${port}`);
-})
+});
